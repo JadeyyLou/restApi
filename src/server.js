0d47fc3e94ffe72ef79b51006e6f7344f8b807ad
Jade-louise simm <Jade-louisesimm1991@outlook.com>
2022-05-11 15:54:18 +0100

    testing login

---
+++ src/server.js
@@ -0,0 +1,17 @@
+require('./db/connection')
+const express = require('express')
+const movieRouter = require('./movie/movieRoutes')
+const userRouter = require('./user/userRoutes')
+const loginRouter = require('./user/loginRoutes')
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(loginRouter);
+app.use(movieRouter);
+app.use(userRouter);
+
+app.listen(port, () => {
+	console.log(` Listening on port ${port}`);
+})
